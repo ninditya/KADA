d85baf4f8869ca98a1bed0d8cbbec027514a9fbb
ninditya <nnditya.salma16@gmail.com>
2026-02-24 15:48:33 +0700

    update index

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.use((req, res, next) => {
-  if (true) {
-    next(new Error('Not Authorized'));
-    // next("Not Authorized");
-    return;
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (true) {
+//     // next(new Error('Not Authorized'));
+//     next("Not Authorized");
+//     return;
+//   }
+//   next();
+// });
 
 app.get('/', (req, res) => {
   res.send('Hello Nindit!')
@@ -26,7 +26,7 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-app.use((err, req, res, next) => {
-  console.log('Error middleware executed:', err.message);
-  res.status(500).send('Internal Server Error');
-});
+// app.use((err, req, res, next) => {
+//   console.log('Error middleware executed:', err.message);
+//   res.status(500).send('Internal Server Error');
+// });
