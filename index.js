1e8583e8ab16a46acd80df765c549e1cd0d08e2e
ninditya <nnditya.salma16@gmail.com>
2026-02-26 16:11:56 +0700

    Ubah koneksi Mongo jadi reusable

--- index.js
+++ index.js
@@ -6,6 +6,23 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 const app = express();
 const port = 3000;
 
