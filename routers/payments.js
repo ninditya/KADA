10b5fef0753375e7a6b1e8a0bee1bcdd7ed44206
ninditya <nnditya.salma16@gmail.com>
2026-03-04 15:29:32 +0700

    coba payment/

--- routers/payments.js
+++ routers/payments.js
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import { createTransaction, createCheckout, handleNotification, getStatus } from '../controllers/paymentsController.js';
 
 const router = Router();
+router.get('/', (req, res) => {
+  res.json({ message: 'Payments endpoint' });
+});
 
 router.post('/transaction', createTransaction); // (Kak Adzki's example endpoint)
 
